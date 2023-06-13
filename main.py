cff8f2fbe0747f5d0de0f1d66c1e0c567812d700
sugi-01096 <132065223+sugi-01096@users.noreply.github.com>
2023-06-14 02:19:40 +0900

    ｔｔｔ

--- main.py
+++ main.py
@@ -1,73 +1,43 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
-import datetime
-import os
-import pickle
-import hashlib
-import uuid
-import random
-if not os.path.exists("data"):
-    os.mkdir("data")
+import json
 
-if not os.path.exists("data/boards.pkl"):
-    with open("data/boards.pkl", "wb") as f:
-        pickle.dump({}, f)
 
-if not os.path.exists("data/threads.pkl"):
-    with open("data/threads.pkl", "wb") as f:
-        pickle.dump({}, f)
+def save_post(title, content):
+    post = {"title": title, "content": content}
+    with open('posts.json', 'a') as file:
+        json.dump(post, file)
+        file.write('\n')
 
-if not os.path.exists("data/users.pkl"):
-    with open("data/users.pkl", "wb") as f:
-        pickle.dump([], f)
-with open("data/boards.pkl", "rb") as f:
-    boards = pickle.load(f)
 
-with open("data/threads.pkl", "rb") as f:
-    threads = pickle.load(f)
+def load_posts():
+    with open('posts.json', 'r') as file:
+        return [json.loads(line) for line in file]
 
-with open("data/users.pkl", "rb") as f:
-    users = pickle.load(f)
-board_names = list(boards.keys())
-board = st.sidebar.selectbox("Select board", board_names)
 
-if st.sidebar.button("New board"):
-    new_board_name = st.sidebar.text_input("Enter board name")
-    if new_board_name != "" and new_board_name not in board_names:
-        boards[new_board_name] = []
-        with open("data/boards.pkl", "wb") as f:
-            pickle.dump(boards, f)
-        board_names.append(new_board_name)
-        board = new_board_name
+def main():
+    st.title("掲示板アプリ")
 
-if board not in boards:
-    st.error("Board not found.")
-    st.stop()
-threads_in_board = [threads[thread_id] for thread_id in boards[board]]
-for thread in threads_in_board:
-    st.write(f"Thread: {thread['title']} ({thread['created_at']})")
-if st.button("New Thread"):
-    thread_title = st.text_input("Thread Title")
-    thread_content = st.text_area("Thread Content")
-    if thread_title != "" and thread_content != "":
-        thread_id = str(uuid.uuid4())
-        user_id = random.randint(1, len(users))
-        thread = {
-            "id": thread_id,
-            "title": thread_title,
-            "content": thread_content,
-            "created_by": users[user_id-1]["username"],
-            "created_at": str(datetime.datetime.now()),
-            "board": board,
-            "comments": []
-        }
-        threads[thread_id] = thread
-        boards[board].append(thread_id)
-        with open("data/threads.pkl", "wb") as f:
-            pickle.dump(threads, f)
-        with open("data/boards.pkl", "wb") as f:
-            pickle.dump(boards, f)
-        st.success("Thread created.")
+    # 新規投稿の入力
+    new_post_title = st.text_input("タイトル")
+    new_post_content = st.text_area("新規投稿", height=100)
+
+    # 投稿ボタンが押された場合
+    if st.button("投稿する") and new_post_title and new_post_content:
+        save_post(new_post_title, new_post_content)
+        st.success("投稿が保存されました！")
+
+    # 保存された投稿の表示
+    posts = load_posts()
+    st.subheader("保存された投稿")
+
+    if not posts:
+        st.info("まだ投稿がありません。")
     else:
-        st.error("Please enter a title and content.")
+        for post in posts:
+            st.text(post["title"])
+            st.text(post["content"])
+            st.markdown("---")
+
+
+if __name__ == "__main__":
+    main()
