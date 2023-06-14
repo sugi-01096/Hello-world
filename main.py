ba2339c6446939e44a8bea4669b3bb46cc5df50a
sugi-01096 <132065223+sugi-01096@users.noreply.github.com>
2023-06-14 16:15:57 +0900

    ＠に置き換え

--- main.py
+++ main.py
@@ -8,8 +8,10 @@ def check_post_content(post_content):
     # 禁止ワードの検出
     for banned_word in banned_words:
         if banned_word in post_content:
-            return True # 禁止ワードが検出された場合はTrueを返す
-    return False # 禁止ワードが検出されなかった場合はFalseを返す
+            # 禁止ワードを＠に置き換える
+            post_content = post_content.replace(banned_word, "＠" * len(banned_word))
+            return post_content, True # 禁止ワードが検出された場合は置き換えた投稿内容とTrueを返す
+    return post_content, False # 禁止ワードが検出されなかった場合は投稿内容とFalseを返す
 
 # 掲示板アプリのメイン処理
 def main():
@@ -21,7 +23,8 @@ def main():
     # 投稿ボタンがクリックされた場合の処理
     if st.button("投稿"):
         # 禁止ワードのチェック
-        if check_post_content(post_content):
+        post_content, banned = check_post_content(post_content)
+        if banned:
             # 禁止ワードが検出された場合は警報を出す
             st.warning("禁止ワードが含まれています！")
         
